909f2491af612b66377a1010b412dac8ea1ec60e
Loïs LARDEAU <lois.lardeau1@epsi.fr>
2018-06-26 10:47:03 +0200

    add comments prononciation in japanese

--- index.php
+++ index.php
@@ -11,7 +11,20 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        /*
+         *  Le 'a' se prononce comme le 'a' en français.
+         *  Le 'i' se prononce comme le 'i' en français.
+         *  Le 'u' se prononce comme le 'ou' en frnaçais.
+         *  Le 'e' se prononce comme le 'é' en français.
+         *  Le 'o' se prononce comme le 'o' en français.
+         *  Le 's' se prononce comme le 's' du mot serpent.
+         *  Le 'sh' se prononce comme le 'ch' en français.
+         *  Le 'ch' se prononce comme le 'tch' en français.
+         *  Le 'h' n'est pas muet en japonais, il se prononce comme en anglais avec une légère expiration.
+         *  Le 'r' est proche du 'l' en français.
+         *  Le 'w' se prononce comme le 'w' en anglais : 'wa'.
+         *  Le 'j' se prononce 'dj' comme dans la marque de voiture Jepp.
+         */
         ?>
     </body>
 </html>
